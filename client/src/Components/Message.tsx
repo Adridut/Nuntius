82b7c860e0ba099f193f5d5b8a6b5d295be7ecaf
unknown <dutfoy.adrien@hotmail.fr>
2021-04-05 12:32:00 +0200

    center messages vertically

--- client/src/Components/Message.tsx
+++ client/src/Components/Message.tsx
@@ -1,10 +1,13 @@
 function Message({ content, author, userName }: any) {
     let message;
     if (author != "Nuntius") {
-        message = <div className={(author === userName) ? "tw-bg-indigo-500 tw-my-2 tw-rounded tw-p-2" : "tw-bg-yellow-500 tw-my-2 tw-rounded tw-p-2"} style={{ width: "fit-content", height: "fit-content" }}>
-            <h6 className="tw-mb-1">{author}</h6>
-            <div className="tw-text-white">{content}</div>
-        </div>
+        message =
+            <div className="tw-flex tw-justify-center">
+                <div className={(author === userName) ? "tw-bg-indigo-500 tw-my-2 tw-rounded tw-p-2" : "tw-bg-yellow-500 tw-my-2 tw-rounded tw-p-2"} style={{ width: "fit-content", height: "fit-content" }}>
+                    <h6 className="tw-mb-1">{author}</h6>
+                    <div className="tw-text-white">{content}</div>
+                </div>
+            </div>
     } else {
 
         message = <div className="tw-bg-red-500 tw-my-2 tw-rounded tw-p-2" style={{ width: "fit-content", height: "fit-content" }}>
