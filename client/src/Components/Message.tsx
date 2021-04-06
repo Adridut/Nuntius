ef71899e32a3e0482c181436b0395496b65a5e87
unknown <dutfoy.adrien@hotmail.fr>
2021-04-06 18:02:59 +0200

    corrected work break bug

--- client/src/Components/Message.tsx
+++ client/src/Components/Message.tsx
@@ -4,8 +4,8 @@ function Message({ content, author, userName }: any) {
         message =
             <div className="tw-flex tw-justify-center">
                 <div className={(author === userName) ? "tw-bg-indigo-300 tw-my-2 tw-rounded tw-p-2" : "tw-bg-yellow-300 tw-my-2 tw-rounded tw-p-2"} style={{ width: "fit-content", height: "fit-content" }}>
-                    <h6 className="tw-mb-1 tw-break-words">{author}</h6>
-                    <div className="tw-text-white tw-break-words">{content}</div>
+                    <h6 className="tw-mb-1 tw-break-all">{author}</h6>
+                    <div className="tw-text-white tw-break-all	">{content}</div>
                 </div>
             </div>
     } else {
@@ -13,7 +13,7 @@ function Message({ content, author, userName }: any) {
         message =
             <div className="tw-flex tw-justify-center">
                 <div className="tw-bg-red-300 tw-my-2 tw-rounded tw-p-2" style={{ width: "fit-content", height: "fit-content" }}>
-                    <div className="tw-text-white tw-break-words">{content}</div>
+                    <div className="tw-text-white tw-break-all">{content}</div>
                 </div>
             </div>
     }
