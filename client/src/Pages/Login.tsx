ee12fc5389e45eb59dec924cdc50ad7735d154ba
unknown <dutfoy.adrien@hotmail.fr>
2021-04-08 16:16:57 +0200

    error messages when username or room name empty

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -9,10 +9,25 @@ import CustomButton from '../Components/CustomButton'
 function Login(props: any) {
     const [room, setRoom] = useState('');
     const [userName, setUserName] = useState('');
+    const [userError, setUserError] = useState('');
+    const [roomError, setRoomError] = useState('');
 
     function handleLogin() {
         // Here, we invoke the callback with the new value
-        props.onChange(true, room, userName);
+        if (room && userName) {
+            props.onChange(true, room, userName);
+        } else {
+            if (!userName) {
+                setUserError("Username cannot be empty.")
+            } else {
+                setUserError("")
+            }
+            if (!room) {
+                setRoomError("Room name cannot be empty.")
+            } else {
+                setRoomError("")
+            }
+        }
     }
 
 
@@ -22,8 +37,10 @@ function Login(props: any) {
                 <div className="tw-w-1/3 tw-flex tw-items-center tw-justify-center">
                     <div>
                         <div className="tw-m-4 tw-mt-8">
-                            <input className="focus:tw-border-yellow-500 tw-mb-3" type="text" placeholder="Username..." onChange={(e) => setUserName(e.target.value)} />
+                            <input className="focus:tw-border-yellow-500" type="text" placeholder="Username..." onChange={(e) => setUserName(e.target.value)} />
+                            <h6 className="tw-text-red-500">{userError}</h6>
                             <input className="focus:tw-border-red-500 tw-mt-3" type="text" placeholder="Room name..." onChange={(e) => setRoom(e.target.value)} />
+                            <h6 className="tw-text-red-500">{roomError}</h6>
                         </div>
                         <div className="tw-flex tw-justify-center tw-items-center">
                             <CustomButton text="Join Room" color="indigo" onClick={handleLogin} />
